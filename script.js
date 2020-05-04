a421c53520336b393aca90a3a0f1c25e506d6f10
Justin Mattson <justin.mattson@outlook.com>
2020-05-04 08:18:33 -0600

    Prevented the video from autoplay & muted video.

--- script.js
+++ script.js
@@ -20,3 +20,9 @@ function showDivs(n) {
   x[slideIndex - 1].style.display = "block";
 }
 // https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_slideshow_self
+
+const video = document.getElementById("video");
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
